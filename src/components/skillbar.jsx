2d03ee9dfd55121c7b79931e0f6e6db3e3eee6f0
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-02-15 08:09:32 +0700

    add skill

--- src/components/skillbar.jsx
+++ src/components/skillbar.jsx
@@ -18,7 +18,7 @@ function SkillBars() {
     <div className='skillbar-container'>
     <h2>Skill</h2>
     <div className='skillbar-wrapper'>
-      <h3>HTML 5 <ProgressBar animated now={85} /> </h3>
+      <h3>HTML 5 <ProgressBar animated now={95} /> </h3>
       <h3>CSS 3<ProgressBar animated now={85} /></h3>
       <h3>JavaScript<ProgressBar animated now={50} /></h3>
       <h3>PHP<ProgressBar animated now={69} /></h3>
