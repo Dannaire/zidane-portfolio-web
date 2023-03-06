72701af11c0c96d57bf8f75c80e67aaef7ffec28
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-03-06 11:20:30 +0700

    add text

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -28,7 +28,8 @@ function Card() {
           <div className="imgCont"> <img src={yoel} />
             <div className="caption">PHP | CSS</div>
             <div className="desc"> 
-            <p>Welcome to our online shoe store, where you can discover the latest trends in footwear and elevate your fashion game. our e-commerce website makes shopping for shoes a breeze.</p>
+            <p>Welcome to our online gadged store, where you can discover the latest trends in gadged wear and elevate your fashion game. our e-commerce website makes shopping for gadget a breeze.</p>
+            
             </div>
           </div>
         </div>
@@ -38,7 +39,7 @@ function Card() {
           <div className="imgCont"> <img src={calc} />
             <div className="caption">React Js | CSS3</div>
             <div className="desc"> 
-            <p>Welcome to our online shoe store, where you can discover the latest trends in footwear and elevate your fashion game. our e-commerce website makes shopping for shoes a breeze.</p>
+            <p>Welcome to our online calculator app, where you can discover the latest trends in calculator app and elevate your calculator game. our calculator website makes demonstration for calculate an math.</p>
             </div>
           </div>
         </div>
